f1a07ac95aff7971e9f326c04a9159dfd49d124f
rahul352001 <rahul@geekersbrain.com>
2021-11-12 01:09:07 +0530

    scroll bar and background image

--- src/components/Home/Clients.js
+++ src/components/Home/Clients.js
@@ -18,38 +18,101 @@ export default function Clients() {
                 alt=""
               />
                </div>
-               <div className="ourClients">
+               <div className="ourClients nivax">
                <img
                 className="clutch"
                 src={require("../../assets/happyClients/image 52.png").default}
                 alt=""
               />
                </div>
-               <div className="ourClients">
+               <div className="ourClients airbnb">
                <img
                 className="clutch"
                 src={require("../../assets/happyClients/image 2.png").default}
                 alt=""
               />
                </div>
-               <div className="ourClients">
+               <div className="ourClients puma">
                <img
                 className="clutch"
                 src={require("../../assets/happyClients/image 3.png").default}
                 alt=""
               />
                </div>
-               <div className="ourClients">
+               <div className="ourClients slack">
                <img
                 className="clutch"
                 src={require("../../assets/happyClients/image 4.png").default}
                 alt=""
               />
                </div>
-               <div className="ourClients">
+               <div className="ourClients uber">
                <img
                 className="clutch"
                 src={require("../../assets/happyClients/image 5.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients sony">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
+                alt=""
+              />
+               </div>
+               <div className="ourClients">
+               <img
+                className="clutch"
+                src={require("../../assets/happyClients/image 6.png").default}
                 alt=""
               />
                </div>
